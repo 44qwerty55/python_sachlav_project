b1926463b51efe0726622c50557db1a68053931b
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2025-03-02 23:59:46 +0300

    add new assert

--- tests/api/test_get_products.py
+++ tests/api/test_get_products.py
@@ -2,10 +2,9 @@ from http import HTTPStatus
 
 from assertpy import assert_that
 
-from data.api.model.get_product_response import ProductsResponse
-
-
 def test_correct_get_products(actual_get_all_products_response, expected_products_response, default_created_product):
     assert_that(actual_get_all_products_response.status_code).is_equal_to(HTTPStatus.OK)
-    assert_that(actual_get_all_products_response.json()).is_equal_to(expected_products_response)
-    assert_that(actual_get_all_products_response.json()).contains(default_created_product.to_dict())
+    actual = actual_get_all_products_response.json()
+    assert_that(actual).is_equal_to(expected_products_response)
+    assert_that(actual).contains(default_created_product.to_dict())
+    assert_that(actual).is_length(20)
